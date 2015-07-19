b760ac8607548846a908227287df7a7b6cc0b268
Ancientgods <devoogdlaurens2@hotmail.com>
2015-07-19 17:40:03 +0200

    Updated for 1.3

--- Trivia/Trivia.cs
+++ Trivia/Trivia.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 
 namespace Trivia
 {
-    [ApiVersion(1,17)]
+    [ApiVersion(1,19)]
     public class Trivia : TerrariaPlugin
     {
         public static TriviaManager TriviaManager = new TriviaManager();
